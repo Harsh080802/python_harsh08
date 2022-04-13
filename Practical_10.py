fca9e481f6f1ec787b98f3cc93e453f5086b50a8
Harsh080802 <90301323+Harsh080802@users.noreply.github.com>
2022-04-13 23:20:18 +0530

    Add files via upload

---
+++ Practical_10.py
@@ -0,0 +1,33 @@
+"""
+Name : Harsh Patel
+ID : 20CS050
+Practical - 10
+"""
+import img2pdf
+from PIL import Image
+
+# Importing the image file
+imp_path = "D:/3rdsem marksheet.png"
+
+# creating a pdf file.
+pdf_path = "D:/marksheet.pdf"
+
+# creating a image file
+image = Image.open(imp_path)
+
+# creating a pdf from the image.
+pdf_bytes = img2pdf.convert(image.filename)
+
+# opening file
+file = open(pdf_path, "wb")
+
+# writing the file.
+file.write(pdf_bytes)
+
+# closing an image.
+image.close()
+
+# closing an file.
+file.close()
+
+print("Success!!")
